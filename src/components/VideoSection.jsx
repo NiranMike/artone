4c1eb61e587bbc5d5ba7814b2c641fb921e94f7e
NiranMike <olaniranmicheal2020@gmail.com>
2024-02-01 10:26:17 +0100

    style: video section

---
+++ src/components/VideoSection.jsx
@@ -0,0 +1,13 @@
+
+const VideoSection = () => {
+  return (
+    <div className="my-[80px] mx-[80px]">
+        <video autoPlay loop muted src="https://framerusercontent.com/assets/6d2J5VD9fc4OA15C9QdlVKngyuU.mp4"></video>
+        <div className="mt-[50px] mb-7">
+            <img src="https://framerusercontent.com/images/eiSoLpXiPIXW6Ufrxkkxa7O5Q.png?scale-down-to=2048" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default VideoSection
